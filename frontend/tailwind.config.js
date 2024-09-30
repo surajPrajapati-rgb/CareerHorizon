5ef4a0ecdac1e7b62217c6bae645406473f0c69d
ashu7531 <ashu7531mehta@gmail.com>
2024-09-30 16:56:27 +0530

    changed style to allow header to utilise full width

--- frontend/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-export const content = [
-  './src/**/*.{js,jsx,ts,tsx}', // Adjust based on where your React components are
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
