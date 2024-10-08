d512b3c82ad7c0593caaf30d805225859d3a3f5d
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2024-10-09 02:35:34 +0530

    added generate_dummy_data files

---
+++ backend/mentorship/management/commands/generate_dummy_data.py
@@ -0,0 +1,10 @@
+# mentoring_app/management/commands/generate_dummy_data.py
+from django.core.management.base import BaseCommand
+from . import create_dummy_data
+
+class Command(BaseCommand):
+    help = "Generate dummy mentor data for testing."
+
+    def handle(self, *args, **kwargs):
+        create_dummy_data(num_records=100)
+        self.stdout.write(self.style.SUCCESS("Successfully created 100 dummy mentor records."))
