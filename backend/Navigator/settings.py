bb76e652d006b0b30dab5e8d45a69c29c86afdeb
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2024-10-08 14:06:27 +0530

    switched back to sqlite3

--- backend/Navigator/settings.py
+++ backend/Navigator/settings.py
@@ -104,17 +104,35 @@ WSGI_APPLICATION = 'Navigator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'CareerNavigator',
+# 		'USER': 'root',
+# 		'PASSWORD': 'mysql23',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 	}
+# }
+
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'CareerNavigator',
-		'USER': 'root',
-		'PASSWORD': 'mysql23',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Creates 'db.sqlite3' in the project root
+    }
 }
 
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'CareerNavigator',
+# 		'USER': 'navigator-admin',
+# 		'PASSWORD': 'TestDB@56789',
+# 		'HOST':'152.58.129.114',
+# 		'PORT':'3306',
+# 	}
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
