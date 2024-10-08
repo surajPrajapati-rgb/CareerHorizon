be5c114363ce11b92cdbe87a3f53db7462d60a24
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2024-10-08 10:21:52 +0530

    fixed the urls and added files for adding dummy data

--- backend/mentorship/urls.py
+++ backend/mentorship/urls.py
@@ -2,9 +2,11 @@
 
 from django.urls import path
 from .views import MentorDetailView, MentorListView
-import mentorship.views as views
+from . import views
 
 urlpatterns = [
+    # path('mentors/', MentorListView.as_view(), name='mentor-list'),
+    path('mentors/<int:mentor_id>/', MentorDetailView.as_view(), name='mentor-detail'),
     path('mentors/', views.mentor_list, name='mentor-list'),
     path('mentors/filter-options/', views.mentor_filter_options, name='mentor-filter-options'),  # New endpoint
 ]
