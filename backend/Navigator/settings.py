051f3eb97e4502a74f54cff784417ba8f76a1292
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2024-11-26 16:49:55 +0530

    added domilas

--- backend/Navigator/settings.py
+++ backend/Navigator/settings.py
@@ -85,7 +85,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React app (local development)
+    "https://celadon-maamoul-63bd57.netlify.app",  # React app (production)
+]
 
 
 ROOT_URLCONF = 'Navigator.urls'
