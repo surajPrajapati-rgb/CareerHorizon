8a95544018481fd80d880051f246ba50f626186e
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2024-11-26 16:07:12 +0530

    changed files

--- backend/Navigator/settings.py
+++ backend/Navigator/settings.py
@@ -146,20 +146,20 @@ WSGI_APPLICATION = 'Navigator.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-# }
-
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'), 
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'), 
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 
 
 # Password validation
