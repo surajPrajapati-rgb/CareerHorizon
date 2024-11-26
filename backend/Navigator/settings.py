0150747d03148ed433fe27dd038d5a6dfc3c4ac6
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2024-11-26 15:13:01 +0530

    added requirments

--- backend/Navigator/settings.py
+++ backend/Navigator/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta 
+import dj_database_url
 
 import os
 from dotenv import load_dotenv
@@ -140,16 +141,23 @@ WSGI_APPLICATION = 'Navigator.wsgi.application'
 #         'PORT': os.getenv('DB_PORT'),
 #     }
 # }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'), 
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'), 
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 
 
 # Password validation
