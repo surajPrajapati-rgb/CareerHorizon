2758720f5958b1473af5abc57e7925280629728c
Suraj Prajapati <117726824+surajPrajapati-rgb@users.noreply.github.com>
2024-11-26 15:23:56 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --no-input
+# python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 
 # python3 manage.py runserver
