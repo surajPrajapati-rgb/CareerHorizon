c5542456698e80ff4d8be693d30c559b49e76cfe
Suraj Prajapati <117726824+surajPrajapati-rgb@users.noreply.github.com>
2024-12-01 09:49:45 +0530

    Update settings.py

--- backend/Navigator/settings.py
+++ backend/Navigator/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'Navigator',
     'mentorship',
     'payment',
-    # 'ProfileSection',
+    'ProfileSection',
     'messaging',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -208,7 +208,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
