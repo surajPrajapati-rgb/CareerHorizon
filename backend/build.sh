92524a8ef47202a58548aa60d34bde6358115cab
Suraj Prajapati <117726824+surajPrajapati-rgb@users.noreply.github.com>
2024-12-01 18:12:22 +0530

    updated build.sh for dynamic variable

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+# Load .env if it exists
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
 
 pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
