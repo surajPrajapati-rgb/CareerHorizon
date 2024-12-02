1e709e7c05c8bc2804de05b7c195b9424bdc2ff7
surajPrajapati-rgb <jnvsuraj7388@gmail.com>
2024-12-02 23:55:33 +0530

    fixed the geting admin user

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('current_user/<str:user>/', views.current_user_view, name='current_user'),
+    path('current_user/', views.current_user_view, name='current_user'),
 ]
